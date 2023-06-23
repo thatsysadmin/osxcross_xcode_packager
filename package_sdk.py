00e97f934544986ba5fbf9a2250f9a9c9b578974
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2023-06-23 04:37:06 -0700

    Initial commit of packaging script

---
+++ package_sdk.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+workspace = sys.argv[1] + "/workroot"
+
+print("Cloning osxcross")
+osx_clone_result = subprocess.check_output("git clone https://github.com/tpoechtrager/osxcross.git", cwd=workspace, shell=True).decode()
+print(osx_clone_result)
+
+print("Packaging Xcode SDK")
+sdk_package_result = subprocess.check_call("./tools/gen_sdk_package.sh", cwd=workspace, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+# print(sdk_package_result)
+
+print("Moving SDK.")
+print(subprocess.check_output("ls -a", cwd=workspace, shell=True))
+
+print("Done.")
+exit(0)
