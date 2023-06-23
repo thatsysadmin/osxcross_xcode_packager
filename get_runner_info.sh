693fa8544e5d18f18f43435e08cf1738fadbd2f8
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2023-06-23 03:23:36 -0700

    careless mistakes

--- get_runner_info.sh
+++ get_runner_info.sh
@@ -2,7 +2,9 @@
 echo "Kernel / System info:"
 uname -a
 echo "Xcode version:"
-xcodebuild -verson
+xcodebuild -version
+echo "Software Update history"
+softwareupdate --history
 echo "Runner HW information:"
 system_profiler SPSoftwareDataType SPHardwareDataType
 echo "Done."
