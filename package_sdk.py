ac202b6dd065dafad486b17c222ffd2f6a3b5817
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2023-06-23 10:11:16 -0700

    Update package_sdk.py

--- package_sdk.py
+++ package_sdk.py
@@ -11,9 +11,10 @@ print("Packaging Xcode SDK")
 sdk_package_result = subprocess.check_call("./osxcross/tools/gen_sdk_package.sh", cwd=workspace, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 # print(sdk_package_result)
 
-print("Moving SDK.")
+print("Moving SDK.\nWorking directory:")
+print(subprocess.check_output("pwd", cwd=workspace, shell=True).decode())
 print(subprocess.check_output("ls -a", cwd=workspace, shell=True).decode())
-subprocess.check_output("mv *.sdk.tar.xz", cwd=workspace, shell=True)
+print(subprocess.check_output("mv *.sdk.tar.xz", cwd=workspace, shell=True).decode())
 
 print("Done.")
 exit(0)
