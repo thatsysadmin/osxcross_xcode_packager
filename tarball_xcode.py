b477e0aae4e1103fe31841e7a60eac3efecb3506
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2023-09-09 00:57:23 -0700

    whoops yet again

--- tarball_xcode.py
+++ tarball_xcode.py
@@ -4,7 +4,7 @@ import sys
 workspace = sys.argv[1] + "/workroot"
 
 print("Tarballing Xcode.")
-xcode_tarball_result = subprocess.check_output("tar -czvf Xcode.tar.gz /Applications/Xcode_15.0.app")
+xcode_tarball_result = subprocess.check_output("tar -czvf Xcode.tar.gz /Applications/Xcode_15.0.app", cwd=workspace, shell=True).decode()
 print(xcode_tarball_result)
 print("Done.")
 exit(0)
